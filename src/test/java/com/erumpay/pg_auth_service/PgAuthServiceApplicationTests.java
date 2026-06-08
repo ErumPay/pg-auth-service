1e1ef0435108607957d578c44043307d06c9f30d
0chotona <eunjongh97@gmail.com>
2026-06-08 12:17:49 +0900

    [KAN-1201] chore: CI 테스트 환경용 DB 설정 추가

--- src/test/java/com/erumpay/pg_auth_service/PgAuthServiceApplicationTests.java
+++ src/test/java/com/erumpay/pg_auth_service/PgAuthServiceApplicationTests.java
@@ -4,10 +4,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(properties = {
-	"spring.autoconfigure.exclude="
-		+ "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,"
-		+ "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration,"
-		+ "org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration"
+	"spring.datasource.url=jdbc:h2:mem:pg_auth_service_test;MODE=MySQL;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
+	"spring.datasource.username=sa",
+	"spring.datasource.password=",
+	"spring.datasource.driver-class-name=org.h2.Driver",
+	"spring.jpa.hibernate.ddl-auto=create-drop",
+	"spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+	"jwt.secret=test-jwt-secret-key-must-be-at-least-256-bits-long-for-hs256",
+	"kakao.client-id=test-kakao-client-id",
+	"internal.api-key=test-internal-api-key"
 })
 class PgAuthServiceApplicationTests {
 
